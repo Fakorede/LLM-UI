834d739cb5bbe26fa28635a2c05a21826435525b
Fakorede Abiola <abiolafakorede@gmail.com>
2024-12-04 19:13:47 -0600

    Increase request payload limit

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ if (process.env.SERVER_ENVIRONMENT == "production") {
     app.use(cors());
 }
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.post("/api/save-chats", (req, res) => {
